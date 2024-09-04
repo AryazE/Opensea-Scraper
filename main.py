e54b1ca067845f3e281ed835199f310128247c3e
Aryaz Eghbali <eghbali.aryaz@gmail.com>
2024-09-04 14:42:06 +0200

    Fixed driver

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ driver.add_experimental_option("prefs", prefs)
 
 
 def get_source_code(asset_id):
-    browser = webdriver.Chrome(driver, options=chrome_options)
+    browser = webdriver.Chrome(driver)
     browser.get(f"https://opensea.io/assets/0x4e76c23fe2a4e37b5e07b5625e17098baab86c18/{asset_id}")
     html = browser.page_source
     browser.close()
